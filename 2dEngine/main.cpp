803a06ec7686aaf596eeb02129eafbc730cb22ad
florianPat <florian.patruck@gmx.de>
2019-11-02 08:22:45 +0100

    rasterization technique documentation

--- 2dEngine/main.cpp
+++ 2dEngine/main.cpp
@@ -57,7 +57,7 @@ int main()
 			obj.verticesClip[i].z = obj.verticesCamera[i].z;
 			// this would also happen here: obj.verticesCamera[i].z /= farPlane; (during perspective projection matrix multiplication)
 
-			//NOTE: One could also use these vars to do it
+			//NOTE: One could also use these vars to do it (but I do not really get this, because the resulting vars do not normalize the coords, how should they?)
 			const float farPlane = 100.0f;
 			const float aspectRatio = ((float)gfx.width) / gfx.height;
 			const float vfov = 90.0f;
@@ -77,11 +77,12 @@ int main()
 			eg::makeZDivide(&obj.verticesClip[i], 1.0f);
 			//NOTE: to normalize z between near and far plane (0 and farPlane in this case. Not really right but yeah). Look at persProj for more information
 			obj.verticesClip[i].z /= farPlane;
-			//TODO: backface culling
+			//backface culling: normal of triangle, normal of lookAt, dot product > 0 -> backface; < 0 -> no backface (visualize
+			//at https://www.mathsisfun.com/sine-cosine-tangent.html)
+			//Because remember: dotProduct = length of A * length of B* cos(angle between them). If they are unit vectors, we get back cos(angle), and
+			//if not, we get back the projection from a on b. Can also use the plus and add formula. (https://www.mathsisfun.com/algebra/vectors-dot-product.html)
 			//TOOD: clip triangles
 			obj.polys[0].clipped = false;
-			//TODO: z-buffer
-			obj.polys[0].visible = true;
 			//viewport space
 			obj.verticesViewport[i] = obj.verticesClip[i] + eg::Vector3f{ 1.0f, 1.0f, 0.0f };
 			obj.verticesViewport[i] *= { gfx.width / 2.0f, gfx.height / 2.0f, 1.0f };
@@ -89,41 +90,21 @@ int main()
 
 		for (int i = 0; i < obj.numPolys; ++i)
 		{
-			if (obj.polys[i].visible && !obj.polys[i].clipped)
+			if (!obj.polys[i].clipped)
 			{
-				//Clip and draw each line
-				for (int j = 0; j < 2; ++j)
-				{
-					eg::Vector3f p03f, p13f;
+				eg::Vector3f p0 = obj.verticesViewport[obj.polys[i].vertexList[0]];
+				eg::Vector3f p1 = obj.verticesViewport[obj.polys[i].vertexList[1]];
+				eg::Vector3f p2 = obj.verticesViewport[obj.polys[i].vertexList[2]];
 
-					p03f = obj.verticesViewport[obj.polys[i].vertexList[j]];
-					p13f = obj.verticesViewport[obj.polys[i].vertexList[j + 1]];
-
-					eg::Vector2i p0, p1;
-					p0 = { (int)p03f.x, (int)p03f.y };
-					p1 = { (int)p13f.x, (int)p13f.y };
-
-					if (eg::clipLine(&p0, &p1, gfx))
-					{
-						// color by fragment shader
-						eg::drawLine(p0, p1, obj.polys[i].color, gfx);
-					}
-				}
-
-				eg::Vector3f p03f, p13f;
-
-				p03f = obj.verticesViewport[obj.polys[i].vertexList[2]];
-				p13f = obj.verticesViewport[obj.polys[i].vertexList[0]];
-
-				eg::Vector2i p0, p1;
-				p0 = { (int)p03f.x, (int)p03f.y };
-				p1 = { (int)p13f.x, (int)p13f.y };
-
-				if (eg::clipLine(&p0, &p1, gfx))
-				{
-					// color by fragment shader
-					eg::drawLine(p0, p1, obj.polys[i].color, gfx);
-				}
+				//NOTE: To interpolate a value from one point to the other we iterate on y, and we want to interpolate x, so for each y, x += dx/dy (or any value 
+				//one wants to interpolate, i.e. u += du/dy)
+				//But this is not perspectivly correct. To correct that, divide when one computes dy, dx, dz by /z. Why dz? Because one also has to interpolate z
+				//the same way. To get the "real" values back, one then has to divide by (1 / z), to get back to "normal" space
+				//bilinear filtering: lerp x and y and use that amout of the pixel color
+				//midmapping:
+				//z-buffer
+				obj.polys[0].visible = true;
+				//Tricks of the game programming gurus, p.1201ff.
 			}
 		}
 
